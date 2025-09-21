96524fb6310a35cb8b37e1b06843a2b233989343
FJS-01 <efejotaese020@gmail.com>
2025-09-21 18:41:41 -0300

    health and ui

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,23 +1,30 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int _health;
-
     [SerializeField] int _lifes;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] int _maxHealth = 5;
+    [SerializeField] int _maxLifes = 1;
+
+    [SerializeField] private Image heartImage;
+
     void Start()
     {
-        _health = 5;
-        _lifes = 1;
+        _health = _maxHealth;
+        _lifes = _maxLifes;
+        UpdateHealthUI();
     }
 
     public void TakeDamage(int dmg)
     {
         _health -= dmg;
+
         if (_health <= 0)
         {
-            
             if (_lifes <= 0)
             {
                 Death();
@@ -25,7 +32,19 @@ public class PlayerHealth : MonoBehaviour
             else
             {
                 _lifes--;
+                _health = _maxHealth; 
             }
+        }
+
+        UpdateHealthUI();
+    }
+
+    private void UpdateHealthUI()
+    {
+        if (heartImage != null)
+        {
+            float porcentaje = (float)_health / _maxHealth;
+            heartImage.fillAmount = porcentaje;
         }
     }
 
